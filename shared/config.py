87e5837e4a606b7e7ff76207abf46202f9e2746d
Anirudh Kakati <anirudhkakati@gmail.com>
2026-04-03 12:41:47 -0600

    added a synchronous version of the database url to be used by alembic

--- shared/config.py
+++ shared/config.py
@@ -4,5 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL=os.getenv("DATABASE_URL")
+DATABASE_URL_SYNC=os.getenv("DATABASE_URL_SYNC")
 REDIS_URL=os.getenv("REDIS_URL")
 ENV=os.getenv("ENV", "dev")
