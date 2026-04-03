f3ada98d8e58883d41a941f24c42c4208b12d888
Anirudh Kakati <anirudhkakati@gmail.com>
2026-04-03 00:05:52 -0600

    initial structure and setup

---
+++ shared/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from config import DATABASE_URL
+from sqlalchemy.orm import DeclarativeBase
+
+asyncio_engine=create_async_engine(DATABASE_URL)
+
+async_session=async_sessionmaker(bind=asyncio_engine,expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
