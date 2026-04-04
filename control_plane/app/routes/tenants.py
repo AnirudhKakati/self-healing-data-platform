84bd084c0d369fb15825a99214cd00d510839dc9
Anirudh Kakati <anirudhkakati@gmail.com>
2026-04-03 20:27:38 -0600

    added endpoints for all CRUD operations for tenants

--- control_plane/app/routes/tenants.py
+++ control_plane/app/routes/tenants.py
@@ -1,13 +1,73 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException, Query 
 from sqlalchemy.ext.asyncio import AsyncSession
 from shared.db import get_db
-from control_plane.app.services.tenants import create_tenant as create_tenant_service
+from control_plane.app.services.tenants import create_tenant_service, get_tenant_service, get_all_tenants_service, delete_tenant_service, update_tenant_service
 from control_plane.app.schemas.tenants import TenantCreate, TenantUpdate, TenantResponse
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from typing import List
 
 router=APIRouter()
 
+#CREATE TENANT
 @router.post("/", response_model=TenantResponse, status_code=status.HTTP_201_CREATED)
 async def create_tenant(tenant_data: TenantCreate, session: AsyncSession = Depends(get_db)):
+
+    try:
+        tenant=await create_tenant_service(tenant_data, session)
+        return tenant
     
-    tenant=await create_tenant_service(session,tenant_data)
-    return tenant
+    except IntegrityError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Tenant could not be created because of a database constraint violation.")
+    except SQLAlchemyError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Database error while creating tenant.")
+
+#GET TENANT BY ID
+@router.get("/{tenant_id}", response_model=TenantResponse)
+async def get_tenant(tenant_id: int, session: AsyncSession=Depends(get_db)):
+
+    tenant_data=await get_tenant_service(tenant_id,session)
+    if not tenant_data:
+        raise HTTPException(status_code=404,detail="Tenant not found. Please check the tenant_id")
+
+    return tenant_data
+
+#GET ALL TENANTS. OPTIONAL LIMIT AND OFFSET
+@router.get("/",response_model=List[TenantResponse])
+async def get_all_tenants(session: AsyncSession=Depends(get_db), limit: int | None=Query(default=None,ge=1), offset: int=Query(default=0, ge=0)): #We user Query() to set constraints and for swagger docs. 
+    #Limit must be greater than equal to 1 and offset greater than equal to 0 
+    try:
+        tenants=await get_all_tenants_service(session, limit, offset)
+        return tenants
+    except SQLAlchemyError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error while fetching tenants.")
+
+#DELETE TENANT BY ID
+@router.delete("/{tenant_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_tenant(tenant_id: int, session: AsyncSession=Depends(get_db)):
+    try:
+        deleted_rows= await delete_tenant_service(tenant_id,session)
+
+        if deleted_rows==0:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tenant not found. Please check the tenant_id")
+        
+        return
+
+    except SQLAlchemyError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error while deleting tenant")
+
+#UPDATE TENANT BY ID
+@router.put("/{tenant_id}", response_model=TenantResponse, status_code=status.HTTP_200_OK)
+async def update_tenant(tenant_id: int, tenant_data: TenantUpdate, session: AsyncSession=Depends(get_db)):
+    try:
+        tenant=await update_tenant_service(tenant_id,tenant_data,session)
+
+        if not tenant:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tenant not found. Please check the tenant_id")
+        
+        return tenant
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=str(e))
+    except IntegrityError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="Tenant could not be updated because of a database constraint violation.")
+    except SQLAlchemyError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Database error while updating tenant.")
--- control_plane/app/services/tenants.py
+++ control_plane/app/services/tenants.py
@@ -1,9 +1,10 @@
 from control_plane.app.models.tenants import Tenant
 from control_plane.app.schemas.tenants import TenantCreate, TenantUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import delete, select
 
-
-async def create_tenant(session: AsyncSession, tenant_data: TenantCreate):
+async def create_tenant_service(tenant_data: TenantCreate, session: AsyncSession):
     tenant_dict=tenant_data.model_dump() #converts the pydantic object to a plain dictionary
     tenant=Tenant(**tenant_dict) #SQLAlchemy constructors expect keyword arguments, so we need to unpack the dictionary with **
 
@@ -13,10 +14,52 @@ async def create_tenant(session: AsyncSession, tenant_data: TenantCreate):
         await session.refresh(tenant) #because the DB generates certain fields, we wait for it to send the latest state to be reflected in the tenant object.
 
         return tenant #we return this full object so we can send it in the Response later
-    except Exception:
-        await session.rollback() #incase of any error, we roll back the session and raise the error
+    except SQLAlchemyError:
+        await session.rollback() #incase of any DB related errors, we roll back the session and raise the error
         raise
 
-async def update_tenant(session: AsyncSession, tenant_data: TenantUpdate):
-    pass
+async def get_tenant_service(tenant_id: int, session: AsyncSession):
+    tenant_data=await session.get(Tenant,tenant_id) #get is used for primary key look up. So the passed tenant_id is mapped to the primary key of the table which is id
+    return tenant_data
+
+async def get_all_tenants_service(session:AsyncSession, limit: int | None=None, offset: int=0): 
+    statement=select(Tenant).order_by(Tenant.id).offset(offset) #we select all tenants with optional offset (0 by default). we also ensure they are sorted by id
+    if limit is not None: #and an optional limit. None by default
+        statement=statement.limit(limit)
+
+    result=await session.execute(statement)
+    tenants=result.scalars().all() #scalars() is used to get clean ORM objects. all() is used to give the results as a list
+    return tenants
+
+async def delete_tenant_service(tenant_id: int, session: AsyncSession):
+    try:
+        result=await session.execute(delete(Tenant).where(Tenant.id==tenant_id)) #try deleting this tenant id and then return the number of rows deleted (should be 1 if deleted, 0 if not deleted)
+        await session.commit()
+        return result.rowcount
+    except SQLAlchemyError: #if any DB related errors, then rollback
+        await session.rollback() 
+        raise
+
+async def update_tenant_service(tenant_id: int, tenant_data: TenantUpdate, session: AsyncSession):
+    
+    tenant=await session.get(Tenant,tenant_id) #first we get the Tenant object
+    if not tenant:
+        return None
+    
+    update_dict=tenant_data.model_dump(exclude_unset=True) #we get the dictionary of the updates. Exclude unset is set to True to avoid updating other fields
+    if not update_dict:
+        raise ValueError("No fields were provided for update.")
+    
+    try:
+        for key,value in update_dict.items():
+            setattr(tenant,key,value) #then set the individual attributes
+
+        await session.commit()
+        await session.refresh(tenant)
+
+        return tenant
+    
+    except SQLAlchemyError: #if any DB error, we rollback
+        await session.rollback()
+        raise
 
