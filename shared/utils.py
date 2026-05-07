10ff65bb1cb86d30c5781d5ded7884a64c1bf619
Anirudh Kakati <anirudhkakati@gmail.com>
2026-05-07 00:30:39 -0600

    moved now_naive as a util function to be used by other modules

--- shared/utils.py
+++ shared/utils.py
@@ -1,5 +1,6 @@
 import hmac,hashlib
 import secrets
+from datetime import datetime
 
 #helper functions
 def generate_secure_key(prefix: str="shdp", length: int=32) -> str:
@@ -10,3 +11,7 @@ def generate_secure_key(prefix: str="shdp", length: int=32) -> str:
 def hash_api_key(api_key: str, secret: str) -> str:
     """Creates a SHA-256 HMAC hash of the API key."""
     return hmac.new(secret.encode(), api_key.encode(), hashlib.sha256).hexdigest()
+
+def now_naive():
+    # helper function to get current timezone-naive time
+    return datetime.now().replace(tzinfo=None)
